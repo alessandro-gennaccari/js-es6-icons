a61bedb8147f8645c76ccc0e96f8b34896b80ac8
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-22 14:25:02 +0100

    array-types

--- js/script.js
+++ js/script.js
@@ -1,3 +1,122 @@
+/* Partendo da questo array di oggetti:
+Aggiungere i colori ad ogni categoria: blue per gli animali, orange per i vegetali e viola per gli utenti. Prendete i colori da un altro array.
+Stampare poi tutte le icone utilizzando il template literal.
+Stampare quindi nella select tutti i tipi che avete in precedenza selezionato (animal, vegetable, icon).
+Filtrare i risultati in base alla categoria (ricordate di svuotare il container).
+Utilizzate forEach, map e filter e cercate di strutturare tutto con le funzioni. */
+
 $(document).ready(function(){
+
+    const icons = [
+        {
+          name: 'cat',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'crow',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dog',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dove',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dragon',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'horse',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'hippo',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'fish',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'carrot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'apple-alt',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'lemon',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'pepper-hot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'user-astronaut',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-graduate',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-ninja',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-secret',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        }
+    ];
+    
+    // Creo un array per salvarmi automaticamente, tutte le possibiili proprietà type presenti nell'array icons
+    const types = [];
+
+    icons.forEach((element) => {
+        if (!types.includes(element.type)){
+            types.push(element.type);
+        }
+    });
+    console.log(types);
+
+    // In base al risultato ottenuto, mi creo un array con i colori che vorrò associare
+    const color = ['blue','orange','purple'];
 
 });
